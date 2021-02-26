b23adce8e30f80adb1bad7aada10f2daca4b61b5
nari2239 <73570767+nari2239@users.noreply.github.com>
2021-02-26 23:13:06 +0900

    posts/aboutのパスを追加(詳細ページのパスの後に「,」を入れ忘れており、一時エラーに悩んだ)

--- myblogapp/urls.py
+++ myblogapp/urls.py
@@ -22,5 +22,6 @@ from posts import views
 urlpatterns = [
     url(r'^posts/', include('posts.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/(?P<post_id>[0-9]+)/$', views.post_detail, name="post_detail")
+    url(r'^posts/(?P<post_id>[0-9]+)/$', views.post_detail, name="post_detail"),
+    url(r'^posts/about/$', views.about, name="about")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
